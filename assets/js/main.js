d11aa1957b98b7fc4645acec9c7bbeb4bd1ed55d
sysoev84l <sysoev84l@ya.ru>
2022-03-03 22:02:04 +0300

    jews

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let currentDate = new Date;
+let currentYear = currentDate.getFullYear();
+let years = document.getElementById('years');
+if (currentYear > 2020) years.innerHTML = '2020 - ' + currentYear
+	else years.innerHTML = '2020'
+let hostName = document.location.origin;
+let hostNameLink = document.querySelector('.host-name');
+hostNameLink.innerHTML = hostName;
