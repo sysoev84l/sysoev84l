8767d4d9ab08a9b9a370015f609f68df40da7cf1
Senior full stack developer <sysoev84l@ya.ru>
2025-01-05 01:50:58 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ let currentYear = currentDate.getFullYear();
 let years = document.getElementById('years');
 if (currentYear > 2020) years.innerHTML = '2020 - ' + currentYear
 	else years.innerHTML = '2020'
-let hostName = document.location.origin;
+let hostName = document.location.href;
 let hostNameLink = document.querySelector('.host-name');
 hostNameLink.innerHTML = hostName;
